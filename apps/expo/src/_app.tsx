911b05e96b67813c6da53f5409a49eedaa531e52
zachuri <punsalang.zachary@gmail.com>
2022-12-06 11:06:17 -0800

    expo: setting up auth screen

--- apps/expo/src/_app.tsx
+++ apps/expo/src/_app.tsx
@@ -1,15 +1,18 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useState } from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { TRPCProvider } from "./utils/trpc";
 
 import { HomeScreen } from "./screens/home";
+import { AuthScreen } from "./screens/auth";
 
 export const App = () => {
+  const [signIn, setSignIn] = useState(true);
+
   return (
     <TRPCProvider>
       <SafeAreaProvider>
-        <HomeScreen />
+        {signIn ? <AuthScreen /> : <HomeScreen />}
         <StatusBar />
       </SafeAreaProvider>
     </TRPCProvider>
