a60017f87fbe50c7c4db4bdd3d5aef18f2ad7445
zachuri <punsalang.zachary@gmail.com>
2022-12-09 13:04:22 -0800

    screens/auth: removed login/ singin: removed image

--- apps/expo/src/screens/auth/signin.tsx
+++ apps/expo/src/screens/auth/signin.tsx
@@ -1,5 +1,4 @@
 import { NavigationProp } from "@react-navigation/native";
-import * as AuthSession from "expo-auth-session";
 import React, { useState } from "react";
 import {
   Text,
@@ -9,6 +8,9 @@ import {
   View,
   Keyboard,
   Alert,
+  Image,
+  KeyboardAvoidingView,
+  Platform,
 } from "react-native";
 import { SafeAreaView, useSafeAreaFrame } from "react-native-safe-area-context";
 import { supabase, supabaseUrl } from "../../lib/supabase";
@@ -34,72 +36,76 @@ export const SignInScreen: React.FC<Prop> = ({ navigation }) => {
 
   return (
     <SafeAreaView className="bg-[#2e026d] bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-      <View className="mx-auto h-full w-full p-4">
-        {/* Title */}
-        <View className="py-2">
-          <Text className="text-4xl text-white">Sign In</Text>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+      >
+        <View className="mx-auto h-full w-full p-4">
+          {/* Title */}
+          <View className="py-2">
+            <Text className="text-4xl text-white">Sign In</Text>
+          </View>
+
+          <View className="mx-2">
+            {/* Email */}
+            <Text className="text-2xl text-white">Email</Text>
+            <TouchableWithoutFeedback
+              onPress={Keyboard.dismiss}
+              accessible={false}
+            >
+              <View className="py-2">
+                <TextInput
+                  className="mb-2 rounded border-2 border-gray-500 p-2 text-white"
+                  placeholder="Email"
+                  placeholderTextColor={"gray"}
+                  onChangeText={(text) => setEmail(text)}
+                />
+              </View>
+            </TouchableWithoutFeedback>
+
+            {/* Password */}
+            <Text className="text-2xl text-white">Password</Text>
+            <TouchableWithoutFeedback
+              onPress={Keyboard.dismiss}
+              accessible={false}
+            >
+              <View className="py-2">
+                <TextInput
+                  className="mb-2 rounded border-2 border-gray-500 p-2 text-white"
+                  placeholder="Password"
+                  placeholderTextColor={"gray"}
+                  onChangeText={(text) => setPassword(text)}
+                  secureTextEntry={true}
+                />
+              </View>
+            </TouchableWithoutFeedback>
+
+            <TouchableOpacity
+              className="mt-5 rounded bg-[#cc66ff] p-2"
+              onPress={() => {
+                signInWithEmail();
+              }}
+            >
+              <Text className="p-2 text-center font-semibold text-white">
+                Sign In
+              </Text>
+            </TouchableOpacity>
+          </View>
+
+          {/* Sign Up / In */}
+          <View className="mt-20">
+            <TouchableOpacity
+              className="rounded p-2"
+              onPress={() => {
+                navigation.navigate("SignUp");
+              }}
+            >
+              <Text className="p-2 text-center font-semibold text-blue-500">
+                Don't have and account? Sign up
+              </Text>
+            </TouchableOpacity>
+          </View>
         </View>
-
-        <View className="mx-2">
-          {/* Email */}
-          <Text className="text-2xl text-white">Email</Text>
-          <TouchableWithoutFeedback
-            onPress={Keyboard.dismiss}
-            accessible={false}
-          >
-            <View className="py-2">
-              <TextInput
-                className="mb-2 rounded border-2 border-gray-500 p-2 text-white"
-                placeholder="Email"
-                placeholderTextColor={"gray"}
-                onChangeText={(text) => setEmail(text)}
-              />
-            </View>
-          </TouchableWithoutFeedback>
-
-          {/* Password */}
-          <Text className="text-2xl text-white">Password</Text>
-          <TouchableWithoutFeedback
-            onPress={Keyboard.dismiss}
-            accessible={false}
-          >
-            <View className="py-2">
-              <TextInput
-                className="mb-2 rounded border-2 border-gray-500 p-2 text-white"
-                placeholder="Password"
-                placeholderTextColor={"gray"}
-                onChangeText={(text) => setPassword(text)}
-                secureTextEntry={true}
-              />
-            </View>
-          </TouchableWithoutFeedback>
-
-          <TouchableOpacity
-            className="mt-5 rounded bg-[#cc66ff] p-2"
-            onPress={() => {
-              signInWithEmail();
-            }}
-          >
-            <Text className="p-2 text-center font-semibold text-white">
-              Sign In
-            </Text>
-          </TouchableOpacity>
-        </View>
-
-        {/* Sign Up / In */}
-        <View className="mt-20">
-          <TouchableOpacity
-            className="rounded p-2"
-            onPress={() => {
-              navigation.navigate("SignUp");
-            }}
-          >
-            <Text className="p-2 text-center font-semibold text-blue-500">
-              Don't have and account? Sign up
-            </Text>
-          </TouchableOpacity>
-        </View>
-      </View>
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 };
