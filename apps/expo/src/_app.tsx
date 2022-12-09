388acc3f830df2a4af1d8f411bd85f074e0c882c
zachuri <punsalang.zachary@gmail.com>
2022-12-08 16:15:22 -0800

    _app: added auth context provider instead of trpc -> wil fix trpc later

--- apps/expo/src/_app.tsx
+++ apps/expo/src/_app.tsx
@@ -1,13 +1,10 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useEffect, useState } from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import { trpc, TRPCProvider } from "./utils/trpc";
-
-import { HomeScreen } from "./screens/home";
-import { AuthScreen } from "./screens/auth";
 import { Session } from "@supabase/supabase-js";
 import { supabase } from "./lib/supabase";
-import { ProfileScreen } from "./screens/profile";
+import Navigation from "./navigation";
+import { AuthContextProvider } from "./utils/auth-context";
 
 export const App = () => {
   const [session, setSession] = useState<Session | null>(null);
@@ -23,13 +20,16 @@ export const App = () => {
   }, []);
 
   return (
-    <TRPCProvider authToken={session?.access_token as string}>
+    //  <TrpcAuthContextProvider>
+
+    // Created our own Provider
+    // Allows us to pass the user data between screens
+    <AuthContextProvider>
       <SafeAreaProvider>
-        {/* {!session ? <AuthScreen /> : <ProfileScreen session={session}/>} */}
-        {/* Later make session a provider that is global */}
-        {!session ? <AuthScreen /> : <HomeScreen session={session} />}
+        <Navigation />
         <StatusBar />
       </SafeAreaProvider>
-    </TRPCProvider>
+    </AuthContextProvider>
+    // </TrpcAuthContextProvider>
   );
 };
